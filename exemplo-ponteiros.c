decc0544a71e842e652e01ecf73a64d7a7140c26
feSilva <silva.fernandadev@gmail.com>
2024-04-16 21:58:30 -0300

    add exemplo ponteiros

--- exemplo-ponteiros.c
+++ exemplo-ponteiros.c
@@ -2,5 +2,20 @@
 #include <stdlib.h>
 
 void main(){
+    int a = 10;
+    printf("\nEndereco a: %p", &a);
+    printf("\nValor a: %d", a);
 
+    int *p = &a;
+    quadrado(p);
+    p =  &a;
+    printf("\nEndereco p: %p",p);
+    printf("\nValor p: %d", *p);
+
+    quadrado(4,&a);
 }//fim main
+
+void quadrado(int *y){
+    *y = 20;
+
+}
